914ba0911a6a2237b4834b9e2e1f7a133087419a
Jake Franklin <jacob.d.franklin@gmail.com>
2018-01-26 12:59:48 -0700

    Small refactor on how graph is rendered

--- scripts/render/graph.js
+++ scripts/render/graph.js
@@ -1,47 +1,53 @@
-const svg = d3.select("svg");
-const w = svg.node().getBoundingClientRect().width;
-const h = svg.node().getBoundingClientRect().height;
-const p = 20;
+class Graph {
+    constructor(maison) {
+        this.maison = maison;
+        this.graph = maison.generate("foyer");
+        this.svg = d3.select("#graph");
+        this.w = this.svg.node().getBoundingClientRect().width;
+        this.h = this.svg.node().getBoundingClientRect().height;
+        this.p = 20;
 
+        // Set up data
+        this.root = d3.hierarchy(this.graph);
+        this.tree = d3.tree().size([this.w - this.p, this.h - this.p]);
 
+        // Set up visualization
+        this.vis = this.svg.append("g")
+            .attr("transform", `translate(${this.p / 2},${this.p / 2})`);
+        this.lineShapeGenerator = d3.linkHorizontal()
+            .x(d => d.x)
+            .y(d => d.y);
 
-const vis = svg.append("g")
-    .attr("transform", `translate(${p / 2},${p / 2})`);
+        this.render();
+    }
 
-// Data
-const maison = new Maison();
-const graph = maison.generate("foyer");
-const root = d3.hierarchy(graph);
-const tree = d3.tree().size([w - p, h - p]);
+    render() {
+        let data = this.tree(this.root);
+        this.vis.selectAll(".line")
+            .data(data.links())
+            .enter()
+            .append("path")
+            .attr("class", "line")
+            .attr("d", this.lineShapeGenerator);
 
-// Viz
-const lineShapeGenerator = d3.linkHorizontal()
-    .x(d => d.x)
-    .y(d => d.y);
+        this.vis.selectAll(".room")
+            .data(data.descendants())
+            .enter()
+            .append("circle")
+            .attr("class", "room")
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y)
+            .attr("r", d => Math.sqrt(d.data.width * d.data.height));
 
-vis.selectAll(".line")
-    .data(tree(root).links())
-    .enter()
-    .append("path")
-    .attr("class", "line")
-    .attr("d", lineShapeGenerator);
-
-vis.selectAll(".room")
-    .data(tree(root).descendants())
-    .enter()
-    .append("circle")
-    .attr("class", "room")
-    .attr("cx", d => d.x)
-    .attr("cy", d => d.y)
-    .attr("r", d => Math.sqrt(d.data.width * d.data.height));
-
-vis.selectAll(".room-label")
-    .data(tree(root).descendants())
-    .enter()
-    .append("text")
-    .attr("class", "room-label")
-    .attr("x", d => d.x)
-    .attr("y", d => d.y)
-    .attr("dy", p / 4)
-    .attr("dx", p / 2)
-    .text(d => d.data.name);
+        this.vis.selectAll(".room-label")
+            .data(data.descendants())
+            .enter()
+            .append("text")
+            .attr("class", "room-label")
+            .attr("x", d => d.x)
+            .attr("y", d => d.y)
+            .attr("dy", this.p / 4)
+            .attr("dx", this.p / 2)
+            .text(d => d.data.name);
+    }
+}
