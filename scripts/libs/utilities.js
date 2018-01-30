3e1d0f32769bfa55b9c7479d5f19a317133832bb
Jake Franklin <jacob.d.franklin@gmail.com>
2018-01-29 20:46:48 -0700

    Added utility for logging tiles to console

--- scripts/libs/utilities.js
+++ scripts/libs/utilities.js
@@ -1,3 +1,26 @@
 Math.getRandomInRange = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+consoleLogGrid = function(grid, field, items, z, hlX, hlY) {
+    var string = "";
+    for (var y = 0; y < grid[0].length; y++) {
+        for (var x = 0; x < grid.length; x++) {
+            if(x && y && x === hlX && y === hlY) // Begin highlight
+                string += "%c";
+            if(items && z !== undefined && items[x + "," + y + "," + z])
+                string += items[x + "," + y + "," + z][0]._char;
+            else if(!grid[x] || !grid[x][y])
+                string += " ";
+            else if(field)
+                string += String(grid[x][y][field]);
+            else
+                string += String(grid[x][y]);
+
+            if(x && y && x === hlX && y === hlY) // close highlight
+                string += "%c";
+        }
+        string += "\n";
+    }
+    console.log(string, "color: red; background: black", "");
+};
