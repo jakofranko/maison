17f38c5e76c3a8cd48de7ef8be970272de8a0fd8
Jake Franklin <jacob.d.franklin@gmail.com>
2018-09-28 13:06:42 -0700

    linting

--- scripts/render/2d/tile.js
+++ scripts/render/2d/tile.js
@@ -44,17 +44,3 @@ Tile.prototype.getDescription = function() {
 Tile.prototype.getName = function() {
     return this._name;
 };
-getNeighborPositions = function(x, y) {
-    var tiles = [];
-    // Generate all possible offsets
-    for (var dX = -1; dX < 2; dX ++) {
-        for (var dY = -1; dY < 2; dY++) {
-            // Make sure it isn't the same tile
-            if (dX == 0 && dY == 0) {
-                continue;
-            }
-            tiles.push({x: x + dX, y: y + dY});
-        }
-    }
-    return tiles.randomize();
-}
